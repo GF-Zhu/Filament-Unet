0a2a7359eead47c23be9c8de5bfe9f71fbc9c1b1
Your Name <you@example.com>
2021-01-24 11:43:52 +0800

    new

--- filament_predict.py
+++
@@ -1,9 +0,0 @@
-from filament_model import *
-from data import *
-import pydotplus
-
-testGene = testGenerator("data/filament/test")
-model = filament_unet()
-model.load_weights("filament_unet.hdf5")
-results = model.predict_generator(testGene,10,verbose=1)
-saveResult("data/filament/test",results)
